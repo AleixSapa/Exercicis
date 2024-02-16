14c9ae6c05601df14a9c4943d9b40d21543e1d2b
Esanpons <ecs200@hotmail.com>
2024-02-16 12:04:51 +0100

    Fer Tot El JavaScript

---
+++ Empresa XYZ/Script.js
@@ -0,0 +1,11 @@
+function ArrayXYZ(Array){
+    var SumaDeArray =  Array[0]+ Array[1] + Array[2]+ Array[3]+ Array[4]+ Array[5]+ Array[5]+ Array[6]+ Array[7]+ Array[8]
+    return SumaDeArray;
+}
+
+
+function Buto1() {
+    Array=[685,684,654,828,651,846,123,456,789];
+    var guardarResultado = ArrayXYZ(Array);
+    alert('Aquet Es El Resultat Del Array '+guardarResultado)
+}
